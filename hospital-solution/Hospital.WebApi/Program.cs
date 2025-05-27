b9feec44aa55bd373da855c1798683ed8d6de4d8
PietroDC <dcpietro78@gmail.com>
2025-05-27 16:55:59 +0200

    added swagger

--- hospital-solution/Hospital.WebApi/Program.cs
+++ hospital-solution/Hospital.WebApi/Program.cs
@@ -12,6 +12,7 @@ internal class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        builder.Services.AddSwaggerGen();
         // Bind configuration section to a strongly-typed class
         builder.Services.Configure<DepartmentConfigurations>(
             builder.Configuration.GetSection("DepartmentConfigurations"));
@@ -26,29 +27,6 @@ internal class Program
         // Register validation rules via extension method
         builder.Services.AddValidationRules();
 
-
-        //// Register rules
-        //builder.Services.AddSingleton(provider =>
-        //{
-        //    //var departments = new List<string> { "Surgey", "Radiology", "Physiotherapy" };
-        //    var config = provider.GetRequiredService<IOptions<DepartmentConfigurations>>().Value;
-        //    return new ReferralRule(config.ReferralDepartments);
-        //});
-
-        //builder.Services.AddSingleton(provider =>
-        //{
-        //    //var departments = new List<string> { "Physiotherapy" };
-        //    var config = provider.GetRequiredService<IOptions<DepartmentConfigurations>>().Value;
-        //    return new InsuranceApprovalRule(config.FinancialApprovalDepartments);
-        //});
-
-        //builder.Services.AddSingleton(provider =>
-        //{
-        //    //var departments = new List<string> { "Radiology" };
-        //    var config = provider.GetRequiredService<IOptions<DepartmentConfigurations>>().Value;
-        //    return new FinancialApprovalRule(config.FinancialApprovalDepartments);
-        //});
-
         builder.Services.AddSingleton<AssignedToGpRule>();
 
         // Register the factory
@@ -56,8 +34,13 @@ internal class Program
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(); 
+        }
 
+        // Configure the HTTP request pipeline.
         app.UseHttpsRedirection();
 
         app.MapPost("/appointments", async (AppointmentRequest request, AppointmentService appointmentService) =>
